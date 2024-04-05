1b6a98e9d968d1507063ce181e0fa29e7682ed92
wogjs5656 <wogjs5656@gmail.com>
2024-04-05 20:53:01 +0900

    fix: import 수정

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -1,13 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  Query,
-} from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { NewsService } from './news.service';
 import { CreateNewsDto } from './dto/create-news.dto';
 
