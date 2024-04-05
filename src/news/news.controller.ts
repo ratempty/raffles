bf540321f439520218b15c9997528bea875c005d
wogjs5656 <wogjs5656@gmail.com>
2024-04-05 20:29:24 +0900

    feat: 신발 뉴스 스크래핑, 조회, 상세조회, 최신, 인기순 정렬

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { NewsService } from './news.service';
 import { CreateNewsDto } from './dto/create-news.dto';
@@ -13,4 +14,36 @@ import { CreateNewsDto } from './dto/create-news.dto';
 @Controller('news')
 export class NewsController {
   constructor(private readonly newsService: NewsService) {}
+
+  @Get('footwear')
+  async getHTML() {
+    return this.newsService.getHTML();
+  }
+
+  @Get('scrapeAll')
+  async scrapeAll() {
+    return this.newsService.scrapeAll();
+  }
+
+  @Get()
+  async findAllNews() {
+    return this.newsService.findAllNews();
+  }
+
+  @Get(':id')
+  async findOneNews(@Param('id') id: number) {
+    await this.newsService.viewCount(id);
+    return this.newsService.findOneNews(id);
+  }
+
+  @Get()
+  async findNews(@Query('type') type: string) {
+    if (type === 'popular') {
+      return this.newsService.findPopularNews();
+    } else if (type === 'latest') {
+      return this.newsService.findLatestNews();
+    } else {
+      return this.newsService.findAllNews();
+    }
+  }
 }
--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -1,5 +1,163 @@
 import { Injectable } from '@nestjs/common';
-import { CreateNewsDto } from './dto/create-news.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { News } from './entities/news.entity';
+import { Repository } from 'typeorm';
+import axios, { all } from 'axios';
+import cheerioModule from 'cheerio';
+import puppeteer from 'puppeteer';
 
 @Injectable()
-export class NewsService {}
+export class NewsService {
+  constructor(
+    @InjectRepository(News)
+    private newsRepository: Repository<News>,
+  ) {}
+  async getHTML() {
+    const options = {
+      method: 'GET',
+      url: 'https://hypebeast.kr/footwear',
+      headers: {
+        accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-language':
+          'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6,zh;q=0.5',
+        referer: 'https://hypebeast.kr/',
+        'sec-ch-ua':
+          '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+      const html = response.data;
+      const $ = cheerioModule.load(html);
+
+      const urls = [];
+      $('.post-box').each(function () {
+        const url = $(this).attr('data-permalink');
+        urls.push(url);
+      });
+
+      console.log('URLs:', urls);
+
+      return urls;
+    } catch (error) {
+      console.error('Error:', error);
+      return null;
+    }
+  }
+
+  async scrapeData(url: string) {
+    try {
+      const browser = await puppeteer.launch({ headless: true });
+      const page = await browser.newPage();
+      await page.goto(url, { timeout: 60000 });
+      const html = await page.content();
+      const $ = cheerioModule.load(html);
+
+      const title = $('.post-body-title').text().trim();
+      const subTitle = $('.post-body-excerpt').text().trim();
+      let content = $('.post-body-content').text().trim();
+      const image = $('.carousel-cell-image').attr('src');
+
+      await browser.close();
+      content = content.split('더 보기')[0].trim();
+      content = content.replace(/\s+/g, ' ');
+      console.log({ title, subTitle, content, image });
+
+      const news = this.newsRepository.create({
+        title,
+        subTitle,
+        content,
+        newsImg: image,
+        newsUrl: url,
+      });
+      await this.newsRepository.save(news);
+
+      return { title, subTitle, content, image };
+    } catch (error) {
+      console.error('Error:', error);
+      return null;
+    }
+  }
+
+  async scrapeAll() {
+    const urls = await this.getHTML(); // getHTML() 메서드를 사용하여 URL을 가져옵니다.
+    if (!urls) return null;
+
+    const scrapedData = [];
+
+    for (const url of urls) {
+      const data = await this.scrapeData(url);
+      if (data) {
+        scrapedData.push(data);
+      }
+    }
+
+    return scrapedData;
+  }
+
+  async findAllNews(): Promise<
+    {
+      title: string;
+      subTitle: string;
+      newsImg: string;
+      views: number;
+    }[]
+  > {
+    const news = await this.newsRepository.find();
+    return news.map((item) => ({
+      title: item.title,
+      subTitle: item.subTitle,
+      newsImg: item.newsImg,
+      views: item.views,
+    }));
+  }
+
+  async findOneNews(id: number): Promise<News> {
+    return this.newsRepository.findOne({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async viewCount(id: number) {
+    const news = await this.newsRepository.findOne({
+      where: {
+        id,
+      },
+    });
+    if (news) {
+      news.views++;
+      await this.newsRepository.save(news);
+    }
+  }
+
+  async findPopularNews() {
+    return this.newsRepository.find({
+      order: {
+        views: 'DESC',
+      },
+      select: ['title', 'subTitle', 'newsImg', 'views'],
+    });
+  }
+
+  async findLatestNews() {
+    return this.newsRepository.find({
+      order: {
+        createdAt: 'DESC',
+      },
+      select: ['title', 'subTitle', 'newsImg', 'views'],
+    });
+  }
+}
