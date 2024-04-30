b95a7334efe35e85dbcd4191681ddf52a4c71827
ratempty <rkdekqls258@naver.com>
2024-04-30 17:50:38 +0900

    feat: 카카오로그인

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,7 @@ async function bootstrap() {
     origin: true,
     credentials: true,
   });
+  app.use(cookieParser());
   await app.listen(3000);
 }
 bootstrap();
--- src/oauth/oauth.controller.ts
+++ src/oauth/oauth.controller.ts
@@ -8,6 +8,7 @@ import {
   Query,
   Res,
 } from '@nestjs/common';
+import { Response } from 'express';
 import { OAuthService } from './oauth.service';
 
 @Controller('kakao')
@@ -24,14 +25,23 @@ export class OAuthController {
   }
   // 사용자 정보 불러오는 로직
   @Get('/oauth/kakao-auth')
-  async getKakaoInfo(@Query() query: { code }) {
+  async getKakaoInfo(
+    @Query() query: { code },
+    @Res({ passthrough: true }) response: Response,
+  ) {
     const KAKAO_REST_API_KEY = process.env.KAKAO_REST_API_KEY;
     const KAKAO_REDIRECT_URI = process.env.KAKAO_REDIRECT_URI;
-    await this.oauthService.kakaoLogin(
+    const kakaoReturn = await this.oauthService.kakaoLogin(
       KAKAO_REST_API_KEY,
       KAKAO_REDIRECT_URI,
       query.code,
     );
-    return { message: '카카오 로그인 되었습니다' };
+    // return { message: '카카오 로그인 되었습니다', kakaoReturn };
+    response.cookie('access_token', kakaoReturn.access_token, {
+      path: '/',
+      secure: true,
+      sameSite: 'none',
+    });
+    response.redirect('https://www.didyouraffles.site/');
   }
 }
